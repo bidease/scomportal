c79bf8bfa3bde5aad31a94963f6d01ae75dac229
Konstantin Kruglov <kruglovk@gmail.com>
2019-10-18 13:12:53 +0300

    support /hosts/<hostID>/traffic_summary

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Base url: `https://portal.servers.com/rest`. Supports:
 - `/stats` (equipment)
 - `/hosts`
 - `/statement/balance`
+- `/hosts/<hostID>/traffic_summary`
---
+++ traffic.go
@@ -0,0 +1,31 @@
+package scomportal
+
+import "fmt"
+
+// BaremetalTraficHost ..
+type BaremetalTraficHost struct {
+	Data struct {
+		Type   string
+		Commit struct {
+			TotalCommitValue            uint    `json:"total_commit_value"`
+			CommitValueForBillingPeriod float64 `json:"commit_value_for_billing_period"`
+			UsageQuantity               string  `json:"usage_quantity"`
+			DateStart                   string  `json:"date_start"`
+			DateEnd                     string  `json:"date_end"`
+		}
+		Overuse struct {
+			UsageQuantity string `json:"usage_quantity"`
+			DateStart     string `json:"date_start"`
+			DateEnd       string `json:"date_end"`
+		}
+	}
+}
+
+// GetBaremetalHostTrafic ..
+func (api *API) GetBaremetalHostTrafic(id uint) (*BaremetalTraficHost, error) {
+	var traffic BaremetalTraficHost
+	if err := api.getRequest(fmt.Sprintf("/hosts/%d/traffic_summary", id), &traffic); err != nil {
+		return nil, err
+	}
+	return &traffic, nil
+}
